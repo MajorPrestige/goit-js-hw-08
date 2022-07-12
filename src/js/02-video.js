6ed93ca79ecbd6d65c8ac0e8476b021519705abb
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-12 15:25:30 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,21 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// const newPlayer = () => {
+//   player.getCurrentTime().then(function (seconds) {
+//     localStorage.setItem('videoplayer-current-time', seconds);
+//   });
+// };
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.on('timeupdate', throttle(newPlayer, 1000));
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
-// player.on('eventName', function (data) {
-//   console.log(data);
-// });
+player.on('timeupdate', throttle(videoDuration, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function videoDuration(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
