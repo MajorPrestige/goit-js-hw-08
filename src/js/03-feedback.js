637525f61a0f28077e0a538cde3d4398bfe7c0d8
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-12 17:11:28 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import localStorageApi from './localstorage';
+import throttle from 'lodash.throttle';
 
+formEl = document.querySelector('.feedback-form');
+inputEmailEl = document.querySelector('input');
+textareaEl = document.querySelector('textarea');
+
+const FEEDBACK_FORM_KEY = 'feedback-data';
+const feedbackFormData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+formInputValue();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorageApi.remove(FEEDBACK_FORM_KEY);
+}
+
+function onFormInput(e) {
+  feedbackFormData[e.target.name] = e.target.value;
+  localStorageApi.save(FEEDBACK_FORM_KEY, feedbackFormData);
+}
+
+function formInputValue() {
+  const inputValue = localStorageApi.load(FEEDBACK_FORM_KEY);
+
+  if (inputValue === undefined) {
+    return;
+  }
+
+  console.log(inputValue);
+  inputEmailEl.value = inputValue.email;
+  textareaEl.value = inputValue.message;
+}
+
+// textareaValue();
+// function onFormInput(e) {
+//   const value = e.target.value;
+//   localStorageApi.save(FEEDBACK_FORM_KEY, value);
+// }
+
+// function textareaValue() {
+//   const savedMessage = localStorageApi.load(FEEDBACK_FORM_KEY);
+
+//   if (savedMessage) {
+//     textareaEl.value = savedMessage;
+//   }
+// }
